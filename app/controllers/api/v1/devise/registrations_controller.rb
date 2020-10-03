bab01cfbed6963d9b52918f6f86c7c8739a79cd1
Dmitry <dimitriy@captureproof.com>
2020-10-03 13:21:45 +0300

    registration controller

---
+++ app/controllers/api/v1/devise/registrations_controller.rb
@@ -0,0 +1,37 @@
+class Devise::RegistrationsController < DeviseController
+  prepend_before_action :require_no_authentication, only: [:new, :create, :cancel]
+  prepend_before_action :authenticate_scope!, only: [:edit, :update, :destroy]
+  prepend_before_action :set_minimum_password_length, only: [:new, :edit]
+  skip_before_filter :verify_authenticity_token, only: :create
+
+  def create
+    build_resource(sign_up_params)
+
+    resource.save
+    yield resource if block_given?
+    if resource.persisted?
+      if resource.active_for_authentication?
+        render json: resource
+      else
+        render json: resource.errors.messages, status: :unprocessable_entity
+      end
+    else
+      render json: resource.errors.messages, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+
+  def ensure_params_exist
+    return if params[:user].present?
+    render json: {
+        messages: "Missing Params",
+        is_success: false,
+        data: {}
+    }, status: :bad_request
+  end
+end
